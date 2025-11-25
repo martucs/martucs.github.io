e170872546424e6100069ffe2b7207438670cddf
Marta <153310274+martucs@users.noreply.github.com>
2025-11-25 20:59:26 +0100

    update 2

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// guardamos todos los <a> links del <nav>
+const navLinks = document.querySelectorAll("nav a");
+
+for (let i = 0; i < navLinks.length; i++) {
+
+  navLinks[i].addEventListener("click", function(event) {
+
+    event.preventDefault(); // prq el default va directo al link
+    const sectionId = this.getAttribute("href");
+    const sectionElement = document.querySelector(sectionId); // find the section element
+
+    console.log("Clicked navbar link:", this.textContent);
+
+    if (sectionElement) {
+      sectionElement.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+}
